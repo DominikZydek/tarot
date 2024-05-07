610ee6473d1334f221063d604990163d17678219
DominikZydek <dominikzydek@gmail.com>
2024-05-07 14:31:32 +0200

    Add url to each card in cards.json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    async function fetchCards() {
+      const response = await fetch("http://localhost:3001/cards");
+      const data = await response.json();
+      setCards(data);
+    }
+    fetchCards();
+  }, []);
+
   return (
     <div className="App">
       <h1 className="text-9xl">Hello, world</h1>
+      <div>
+        {cards.map(card => (
+          <div key={card.id}>
+            <h2>{card.name}</h2>
+            <img src={card.url} alt={card.name} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
